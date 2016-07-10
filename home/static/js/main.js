4205192066fcb28b4ed8fbc790f66ad30f732b4d
Samuel Murillo <samuelmurillo333@gmail.com>
2016-07-09 22:27:45 -0400

    Desktop and resourses panel added

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,1 +1,16 @@
+/*$(document).ready(function () {
+    var desktop = $('#desktop');
+    $(window).height();
+});*/
 
+$(document).ready(function () { resizeDiv(); });
+
+window.onresize = function (event) { resizeDiv(); }
+
+function resizeDiv() {
+    //vpw = $(window).width();
+    vph = $(window).height();
+    vph = vph - $('.mdl-mini-footer').outerHeight()
+    $('#desktop').css({'height':vph + 'px'});
+    $('#desktop-background').css({'height':vph + 'px'});
+}
