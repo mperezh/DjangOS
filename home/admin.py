08315c1d312ffcdb8d26bae25f84d1f27fcace4d
Samuel Murillo <samuelmurillo333@gmail.com>
2016-07-27 20:17:48 -0400

    Add App model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from home.models import App
 
-# Register your models here.
+admin.site.register(App)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class App(models.Model):
+    app_id = models.CharField(max_length=60, default="")
+    app_name = models.CharField(max_length=60)
+    cpu_use = models.DecimalField(max_digits=5, decimal_places=2)
+    memory_use = models.DecimalField(max_digits=5, decimal_places=2)
+    disk_use = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.app_name
+
