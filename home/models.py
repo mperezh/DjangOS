6e768fa77b1d5de0013d33cba07dc7bcc8b19ef9
Samuel Murillo <samuelmurillo333@gmail.com>
2016-08-17 18:20:05 -0400

    Make cpu, memory and disk usage integers

--- home/models.py
+++ home/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class App(models.Model):
     app_id = models.CharField(max_length=60, default="")
     app_name = models.CharField(max_length=60)
-    cpu_use = models.DecimalField(max_digits=5, decimal_places=2)
-    memory_use = models.DecimalField(max_digits=5, decimal_places=2)
-    disk_use = models.DecimalField(max_digits=5, decimal_places=2)
+    cpu_use = models.IntegerField(default=0)
+    memory_use = models.IntegerField(default=0)
+    disk_use = models.IntegerField(default=0)
 
     def __str__(self):
         return self.app_name
