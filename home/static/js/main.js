985c1641d9b9632c55d7e16b0906d66db1acbfe4
Samuel Murillo <samuelmurillo333@gmail.com>
2016-08-31 22:52:00 -0400

    Use for statement for jQuery selectors

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -14,58 +14,11 @@ $(document).ready(function () {
     resizeDiv();
     $("#processes-table").load("reports/processes/add/system");
     $("#memory-table").load("reports/memory-table/show");
-    setTimeout(function() {
+    setTimeout(function () {
         $("#resources").load("reports/resources");
     }, 100);
-});
-
-$('.chrome-app').click(function () {
-
-    $("#memory-table").load("reports/memory-table/add/chrome");
-    setTimeout(function() {
-        $("#processes-table").load("reports/processes/add/chrome");
-    }, 100);
-
-    setTimeout(function() {
-        $("#resources").load("reports/resources");
-    }, 100);
-
-    $("#desktop").load("apps/open/chrome");
-    resizeDiv();
-});
-
-$('.folders-app').click(function () {
-
-    $("#memory-table").load("reports/memory-table/add/folders");
-    setTimeout(function() {
-        $("#processes-table").load("reports/processes/add/folders");
-    }, 100);
-
-    setTimeout(function() {
-        $("#resources").load("reports/resources");
-    }, 100);
-
-    $("#desktop").load("apps/open/folders");
-    resizeDiv();
-});
-
-$('.calculator-app').click(function () {
-
-    $("#memory-table").load("reports/memory-table/add/calculator");
-    setTimeout(function() {
-        $("#processes-table").load("reports/processes/add/calculator");
-    }, 100);
-
-    setTimeout(function() {
-        $("#resources").load("reports/resources");
-    }, 100);
-
-    $("#desktop").load("apps/open/calculator");
-    resizeDiv();
 });
 
 $('#compact').click(function () {
-    setTimeout(function() {
-        $("#memory-table").load("/reports/memory-table/compact");
-    }, 100);
+    $("#memory-table").load("/reports/memory-table/compact");
 });
